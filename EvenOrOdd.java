d0364eb38f948bf0482cca967fd3052c5cded2d5
Ahsanul Karim Tanim <tanim494@gmail.com>
2022-10-31 23:41:35 +0600

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+       for(int i=1;i<20;i++){
+           if (i%2==0){
+               System.out.println(i+"-Even");
+           } else {
+               System.out.println(i+"-Odd");
+           }
+       }
+
+    }
+}
